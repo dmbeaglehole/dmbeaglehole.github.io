5866570506372c9081e07f49070d9271c611ffb3
dmbeaglehole <84294223+dmbeaglehole@users.noreply.github.com>
2025-10-06 03:54:48 +0000

    Deploying to gh-pages from @ dmbeaglehole/dmbeaglehole.github.io@d66a8fcd69a3b9d274426cc759b560dd34d20c40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -460,30 +460,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://scholar.google.com/citations?user=wO-FY_4AAAAJ", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
